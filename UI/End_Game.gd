b2bc8f163e988549fc6b02ab9839a3b5964ea304
DomBenne <97532864+DomBenne@users.noreply.github.com>
2022-04-20 19:49:56 -0400

    P4

--- UI/End_Game.gd
+++ UI/End_Game.gd
@@ -7,9 +7,8 @@ func _ready():
 
 func _on_Play_pressed():
 	Global.reset()
-	var _scene = get_tree().change_scene("res://Level_1.tscn")
+	var _scene = get_tree().change_scene("res://Level1.tscn")
 
 
 func _on_Quit_pressed():
 	get_tree().quit()
-	#
